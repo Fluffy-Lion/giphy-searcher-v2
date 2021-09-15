ab95809d43f6258620d7b175b6626ae8c93cc1eb
Fluffy-Lion <leoncooper31@gmail.com>
2021-09-15 15:13:43 +0100

    dotenv added with api key

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
+import { useState } from "react";
 
 
-function App() {
+const App = () => {
+
+  const [data, setData] = useState([])
+  const [random, setRandom] = useState({})
+  const [loaded, setLoaded] = useState(false)
+  const [favourites, setFavourites] = useState([])
+
+  const getRandom = async () => {
+    const key = process.env.REACT_APP_API_KEY
+
+    try {
+      let response = await fetch(`https://api.giphy.com/v1/gifs/random?api_key=${key}`)
+      let newData = await response.json()
+      setRandom(newData)
+      setLoaded(true)
+    } catch (error) {
+      console.log(error)
+    }
+  }
   return (
-    <h1>giphy searcher v2</h1>
+    <div>
+      <h1>giphy searcher v2</h1>
+      <button onClick={getRandom}>get</button>
+    </div>
   );
 }
 
